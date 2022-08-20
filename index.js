72fb3d3511f6bc7ec00d0df462eeb345330b6a37
adeel085 <adeel.ahmed9360@gmail.com>
2022-08-20 07:36:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/error', async ctx => {
+  ctx.status = 500;
+  throw new Error('This is a test error');
+});
+
+router.get('/', ctx => {
+  ctx.body = "Howdy";
+});
+
+// app.use(sentryMiddleware);
+app.use(router.routes());
+app.listen(process.env.PORT);
